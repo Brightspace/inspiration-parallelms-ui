0dd717dfc12f5467b3e2a6104a687f3265db6806
Anthony Wikkerink <Anthony.Wikkerink@D2L.com>
2017-09-13 09:59:03 -0400

    Cleanup

--- src/link/link-sw.js
+++ src/link/link-sw.js
@@ -3,6 +3,7 @@
 var map = {};
 
 self.addEventListener('message', function(event) {
+	// create a unique link that the anchor tag can use that this SW will intercept
 	var uniqLink = self.registration.scope + 'intercept-me-nr' + Math.random();
 	map[uniqLink] = event.data;
 
@@ -13,17 +14,21 @@ self.addEventListener('message', function(event) {
 self.addEventListener('fetch', function(event) {
 	if (map.hasOwnProperty(event.request.url)) {
 		var data = map[event.request.url];
-		var request = new Request(data.url, {
-			method: event.request.method,
-			headers: event.request.headers,
-			mode: 'cors',
-			credentials: event.request.credentials
-		});
-		Object.keys(data.headers).forEach(function(header) {
-			request.headers.append(header, data.headers[header]);
-		});
+		if (data.headers) {
+			// The request needs to be a cors request to set headers
+			var request = new Request(data.url, {
+				method: event.request.method,
+				headers: event.request.headers,
+				mode: 'cors',
+				credentials: event.request.credentials
+			});
+			Object.keys(data.headers).forEach(function(header) {
+				request.headers.append(header, data.headers[header]);
+			});
+		}
 		event.respondWith(fetch(request)
 			.then(function(response) {
+				// Set Content-Disposition on the response if necessary
 				if (!data.filename) {
 					return;
 				}
