b120da92a12851ee3cd4b2b730f4bab987cc44f6
Lawrence Wong <Lawrence.Wong@D2L.com>
2018-06-20 15:47:31 -0400

    Renmaing add Note to note create, cleaning up some bad Ids, creating the notes by reource and count components

---
+++ src/notes/notes-count.js
@@ -0,0 +1,41 @@
+import { PolymerElement } from '@polymer/polymer/polymer-element.js';
+import { SirenEntityMixin } from '../siren-entity-mixin.js';
+import { html } from '@polymer/polymer/lib/utils/html-tag.js';
+
+/* @mixes SirenEntityMixin */
+class NotesCount extends SirenEntityMixin(PolymerElement) {
+	static get template() {
+		return html`
+		<style>
+            :host {
+                display: block;
+            }
+        </style>
+        Count: [[notesCount]]
+`;
+	}
+
+	static get is() { return 'd2l-notes-count'; }
+
+	static get properties() {
+		return {
+            notesCount: {
+                type: Number,
+                value: 0
+            }
+		};
+	}
+
+	static get observers() {
+		return [
+			'_changed(entity)'
+		];
+	}
+
+	_changed(entity) {
+        if (!entity.properties) return
+        this.notesCount = entity.properties.count;
+	}
+}
+
+window.customElements.define(NotesCount.is, NotesCount);
