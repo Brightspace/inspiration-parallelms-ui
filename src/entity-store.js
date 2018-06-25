eb95855ab7ac174f6be625e82866066f9cf25425
Al Lamarre <Al.Lamarre@D2L.com>
2018-06-25 16:10:42 -0400

    Restore push notifications

--- src/entity-store.js
+++ src/entity-store.js
@@ -1,6 +1,45 @@
 import { GlobalSearch } from './global-search.js';
 
-var listeners = {};
+const listeners = {};
+const updateService = 'https://resource-invalidator.api.dev.brightspace.com';
+
+function fetchMessages() {
+	fetch(`${updateService}/messages`, {credentials: 'include'})
+		.then(function(response) {
+			return response.json();
+		})
+		.then(result => {
+			for (const resource of result.resources) {
+				if (listeners[resource]) {
+					for (var i = 0; i < listeners[resource].length; i++) {
+						const token = listeners[resource][i];
+
+						EntityStore.fetch(resource, token, true);
+					}
+				}
+			}
+			fetchMessages();
+		}).catch(fetchMessages);
+}
+
+const registryPromise = new Promise((resolve) => {
+	function connect() {
+
+		fetch(`${updateService}/register`, {
+			method: 'POST',
+			credentials: 'include'
+		})
+			.then(() =>{
+				resolve();
+				fetchMessages();
+			}, () => {
+				setTimeout(connect, 2000);
+			});
+	}
+
+	connect();
+
+});
 
 function listenForChanges(resource, token)  {
 	if (listeners[resource]) {
@@ -10,6 +49,19 @@ function listenForChanges(resource, token)  {
 	} else {
 		listeners[resource] = [token];
 	}
+
+	registryPromise.then(() => {
+		fetch(`${updateService}/listen`, {
+			method: 'POST',
+			credentials: 'include',
+			body: JSON.stringify({resource: resource}), // must match 'Content-Type' header
+			headers: {
+				'content-type': 'application/json'
+			},
+		});
+	})
+		.catch(() => listenForChanges(resource, token));
+
 }
 
 /*
